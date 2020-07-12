a2f45f72ebaec7fb438cd2663406a2c77f6b414a
Angel Archbold <angelarchbold09@gmail.com>
2020-07-12 16:15:06 -0500

    good practices with serverSideRender

--- src/server/server.js
+++ src/server/server.js
@@ -39,8 +39,9 @@ if (config.dev) {
 }
 
 const setResponse = (html, preloadedState, manifest) => {
-  const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css'
-  const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js'
+  const mainStyles = manifest ? manifest["main.css"] : "assets/app.css";
+  const mainBuild = manifest ? manifest["main.js"] : "assets/app.js";
+  const vendorBuild = manifest ? manifest["vendors.js"] : "assets/vendor.js";
   return `
     <!DOCTYPE html>
 <html lang="en">
@@ -59,6 +60,7 @@ const setResponse = (html, preloadedState, manifest) => {
         )}
         </script>
         <script src="${mainBuild}" type="text/javascript"></script>
+        <script src="${vendorBuild}" type="text/javascript"></script>
     </body>
 </html>
 `;
